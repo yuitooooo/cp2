5ae86701e805c1b0a2450982cae0c0e4f8869a7c
yuitooooo <yuito.sbaseboll@gmail.com>
2023-02-04 20:31:29 +0900

    env building

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,3 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
